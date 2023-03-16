c02a98ef920586a92fb343f61e2f591bee2118c9
JoshXTMX <121321892+JoshXTMX@users.noreply.github.com>
2023-03-16 17:42:02 -0400

    Add files via upload

---
+++ proyecto 1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//for(int i = 1; i <= 10; i++)
+//{
+// Console.WriteLine(i + "Josue Ramirez");
+
+//}
+
+int i = 0;
+
+while (i < 10) ;
+{
+    Console.WriteLine(1);
+    ++i;
+}
